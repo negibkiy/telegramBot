3900610d701e74cc3863951372b0a7c0e226d339
negibkiy <grigona4@mail.ru>
2022-06-29 15:11:30 +0300

    sad

--- python/bot.py
+++ python/bot.py
@@ -7,14 +7,14 @@ BOT_TOKEN = "5581837086:AAFqDJgaaDop64v4cHA7HehlL08RNh-dTFU"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-# mydb = mysql.connector.connect(
-#   host=host,
-#   user=user,
-#   password=password,
-#   database=database
-# )
+mydb = mysql.connector.connect(
+  host=host,
+  user=user,
+  password=password,
+  database=database
+)
 
-# mycursor = mydb.cursor()
+mycursor = mydb.cursor()
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -38,38 +38,59 @@ def start(message):
 def event(message): 
     if message.text == '🚲 Мероприятия':
         bot.send_message(message.from_user.id, "Хай")
+        
     if message.text == '🏢 Консультации':
         bot.send_message(message.from_user.id, "Хай2")
+
     if message.text == '🕒 Заметки':
         bot.send_message(message.from_user.id, "Хай")
+
     if message.text == '🚁 Основные подразделения':
         bot.send_message(message.from_user.id, "Хай")
+
     if message.text == '📂 Полезные ссылки':
         bot.send_message(message.from_user.id, "Хай")
+
     if  message.text == '🏫 Корпуса':
-         bot.send_message(message.from_user.id, "Хай")
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("А учебный корпус")
+        item2 = types.KeyboardButton("Б учебный корпус")
+        item3 = types.KeyboardButton("Высотный учебный корпус")
+        item4 = types.KeyboardButton("Главный учебный корпус")
+        item5 = types.KeyboardButton("Кировский учебный корпус")
+        item6 = types.KeyboardButton("Красноармейский учебный корпус")
+        item7 = types.KeyboardButton("Тракторный учебный корпус")
+        markup.add(item1, item2, item3, item4, item5, item6, item7)
+        bot.send_message(message.from_user.id,"📅 Расписание", reply_markup = markup)
+        bot.register_next_step_handler(message, table)
+
     if message.text == '📅 Расписание':
-        table(message)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Расписание преподавателя")
+        item2 = types.KeyboardButton("Расписание экзаменов")
+        item3 = types.KeyboardButton("Обычное расписание")
+        item4 = types.KeyboardButton("Расписание звонков")
+        markup.add(item1, item2, item3, item4)
+        bot.send_message(message.from_user.id,"📅 Расписание", reply_markup = markup)
+        bot.register_next_step_handler(message, table)
 
 
 def table(message): 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Расписание преподавателя")
-    item2 = types.KeyboardButton("Расписание экзаменов")
-    item3 = types.KeyboardButton("Обычное расписание")
-    markup.add(item1, item2, item3)
-    bot.send_message(message.from_user.id,"asd", reply_markup = markup)
+    if message.text == 'Расписание преподавателя':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("преподавателя")
+        markup.add(item1)
+        bot.send_message(message.from_user.id,"asd", reply_markup = markup)
 
-
-# def build(message):
-#     if  message.text == '🏫 Корпуса':
-#         mycursor.execute(" SELECT * FROM addresses WHERE idaddresses = '3' ")
-#         base = mycursor.fetchall()
-#         for row in base:
-#             text = row[1]
-#             photo = row[2]   #тут можно указать какое поле выбрать из бд
-#         bot.send_message(message.from_user.id, text)
-#         bot.send_photo(message.from_user.id, open(photo, 'rb'))
+def build(message):
+    if  message.text == '🏫 Корпуса':
+        mycursor.execute(" SELECT * FROM addresses WHERE idaddresses = '3' ")
+        base = mycursor.fetchall()
+        for row in base:
+            text = row[1]
+            photo = row[2]   #тут можно указать какое поле выбрать из бд
+        bot.send_message(message.from_user.id, text)
+        bot.send_photo(message.from_user.id, open(photo, 'rb'))
 
 
 bot.polling(none_stop=True)
