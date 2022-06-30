639e0bcc6c2791aa6cb352416437e936d0166dd6
negibkiy <grigona4@mail.ru>
2022-06-30 08:26:21 +0300

    backtomenu

--- python/bot.py
+++ python/bot.py
@@ -144,4 +144,17 @@ def website(messange):
     if message.text == "dsfads":
        bot.register_next_step_handler(message, build)
 
+
+def back_to_main(message):                                     # ФУНКЦИЯ ДЛЯ ВЫЗОВА ГЛАВНОГО МЕНЮ
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("🚲 Мероприятия")
+    item2 = types.KeyboardButton("🏢 Консультации")
+    item3 = types.KeyboardButton("🕒 Заметки")
+    item4= types.KeyboardButton("🚁 Основные подразделения")
+    item5 = types.KeyboardButton("📂 Полезные ссылки")
+    item6 = types.KeyboardButton("🏫 Корпуса") 
+    item7 = types.KeyboardButton("📅 Расписание")
+    markup.add(item1, item2, item3, item4, item5, item6, item7)
+    bot.send_message(message, "Здравствуйте, я - информационный бот VSTU для помощи студентам.", reply_markup = markup)
+
 bot.polling(none_stop=True)
